d4d969b73d980042702fe95aca8cc310a5bb22b4
ahmalla <ahmalla@hotmail.com>
2022-10-20 10:31:45 -0700

    succesfully stacked images ontop of eachother, tested rotate css

--- main.js
+++ main.js
@@ -12,22 +12,22 @@ playerHitpointCount.textContent = playerHitpoints;
 // populate the data for cards
 
 const getData = () => [
-    {imgSrc: 'Images/Ancestral_hat.png', name: 'Hat'},
-    {imgSrc: 'Images/black_hween.png', name: 'hween'},
-    {imgSrc: 'Images/Black_partyhat.png', name: 'partyhat'},
-    {imgSrc: 'Images/christmas_cracker.png', name: 'cracker'},
-    {imgSrc: 'Images/Disk_of_returning.png', name: 'disk'},
-    {imgSrc: 'Images/Dragon_mace.png', name: 'mace'},
-    {imgSrc: 'Images/runescape_cat.png', name: 'cat'},
-    {imgSrc: 'Images/santahat.png', name: 'santa'},
-    {imgSrc: 'Images/Ancestral_hat.png', name: 'Hat'},
-    {imgSrc: 'Images/black_hween.png', name: 'hween'},
-    {imgSrc: 'Images/Black_partyhat.png', name: 'partyhat'},
-    {imgSrc: 'Images/christmas_cracker.png', name: 'cracker'},
-    {imgSrc: 'Images/Disk_of_returning.png', name: 'disk'},
-    {imgSrc: 'Images/Dragon_mace.png', name: 'mace'},
-    {imgSrc: 'Images/runescape_cat.png', name: 'cat'},
-    {imgSrc: 'Images/santahat.png', name: 'santa'},
+    {imgSrc: './Images/Ancestral_hat.png', name: 'Hat' },
+    {imgSrc: './Images/black_hween.png', name: 'hween' },
+    {imgSrc: './Images/Black_partyhat.png', name: 'partyhat' },
+    {imgSrc: './Images/christmas_cracker.png', name: 'cracker' },
+    {imgSrc: './Images/Disk_of_returning.png', name: 'disk' },
+    {imgSrc: './Images/Dragon_mace.png', name: 'mace' },
+    {imgSrc: './Images/runescape_cat.png', name: 'cat' },
+    {imgSrc: './Images/santahat.png', name: 'santa' },
+    {imgSrc: './Images/Ancestral_hat.png', name: 'Hat' },
+    {imgSrc: './Images/black_hween.png', name: 'hween' },
+    {imgSrc: './Images/Black_partyhat.png', name: 'partyhat' },
+    {imgSrc: './Images/christmas_cracker.png', name: 'cracker' },
+    {imgSrc: './Images/Disk_of_returning.png', name: 'disk' },
+    {imgSrc: './Images/Dragon_mace.png', name: 'mace' },
+    {imgSrc: './Images/runescape_cat.png', name: 'cat'} ,
+    {imgSrc: './Images/santahat.png', name: 'santa' },
 ];
 
 // shuffle function
@@ -35,7 +35,6 @@ const shuffle = () => {
     const cardData = getData();
     cardData.sort(() => Math.random() - 0.5);
     return cardData;
-    
 };
 
 // creating cards function
