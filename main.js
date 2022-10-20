e4649855a2e75ab4e6bad0e5360cbdcda156d2d0
ahmalla <ahmalla@hotmail.com>
2022-10-20 07:37:26 -0700

    creating vars and link texts

---
+++ main.js
@@ -0,0 +1,9 @@
+// creating variables
+
+const main = document.querySelector('main');
+const playerHitpointCount = document.querySelector('span');
+const playerHitpoints = 10;
+
+// setting up dom elements
+
+playerHitpointCount.textContent = playerHitpoints;
