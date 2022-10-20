93f47fa1b34c45c2f05766120dcb4274fdaa7260
ahmalla <ahmalla@hotmail.com>
2022-10-20 07:46:15 -0700

    creating object array

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ const playerHitpoints = 10;
 // setting up dom elements
 
 playerHitpointCount.textContent = playerHitpoints;
+
+
+// populate the data for cards
+
+const getData = () => [
+    {imgSrc: 'Images/Ancestral_hat.png', name: 'Hat'},
+    {imgSrc: 'Images/black_hween.png', name: 'hween'},
+    {imgSrc: 'Images/Black_partyhat.png', name: 'partyhat'},
+    {imgSrc: 'Images/christmas_cracker.png', name: 'cracker'},
+    {imgSrc: 'Images/Disk_of_returning.png', name: 'disk'},
+    {imgSrc: 'Images/Dragon_mace.png', name: 'mace'},
+    {imgSrc: 'Images/runescape_cat.png', name: 'cat'},
+    {imgSrc: 'Images/santahat.png', name: 'santa'},
+    {imgSrc: 'Images/Ancestral_hat.png', name: 'Hat'},
+    {imgSrc: 'Images/black_hween.png', name: 'hween'},
+    {imgSrc: 'Images/Black_partyhat.png', name: 'partyhat'},
+    {imgSrc: 'Images/christmas_cracker.png', name: 'cracker'},
+    {imgSrc: 'Images/Disk_of_returning.png', name: 'disk'},
+    {imgSrc: 'Images/Dragon_mace.png', name: 'mace'},
+    {imgSrc: 'Images/runescape_cat.png', name: 'cat'},
+    {imgSrc: 'Images/santahat.png', name: 'santa'},
+];
+
